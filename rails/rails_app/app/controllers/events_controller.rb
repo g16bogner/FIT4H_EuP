db2580f46ac6d17fca95f8c788ada1495349ea90
g16bogner <g16bogner@gmail.com>
2015-11-13 09:37:32 +0100

    show

--- rails/rails_app/app/controllers/events_controller.rb
+++ rails/rails_app/app/controllers/events_controller.rb
@@ -3,4 +3,8 @@ class EventsController < ApplicationController
 	def index
 		@events = Event.all 
 	end
+
+	def show
+		@event = Event.find(params[:id])
+	end
 end
--- rails/rails_app/app/helpers/application_helper.rb
+++ rails/rails_app/app/helpers/application_helper.rb
@@ -1,7 +1,8 @@
 module ApplicationHelper
 	def format_price(event)
 		if event.free? 
-			"FREE"
+			#"<strong>FREE</strong>".html_safe
+			content_tag(:strong, "Free")
 		else
 			number_to_currency(event.price, unit: "€ ") 
 		end
