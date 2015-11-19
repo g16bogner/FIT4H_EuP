f38ed96e5dea2fc4e94fe4474929130a354573b5
g16bogner <g16bogner@gmail.com>
2015-11-19 13:10:57 +0100

    forms

--- rails/rails_app/app/controllers/events_controller.rb
+++ rails/rails_app/app/controllers/events_controller.rb
@@ -8,7 +8,29 @@ class EventsController < ApplicationController
 		@event = Event.find(params[:id])
 	end
 
+	def new
+		@event = Event.new
+	end
+	def create
+		@event = Event.new(event_params)
+		@event.save
+		redirect_to event_path(@event.id)
+	end
 	def edit
 		@event = Event.find(params[:id])
 	end
+	def update
+		@event = Event.find(params[:id])
+		@event.update(event_params)
+		redirect_to event_path(@event.id)
+	end
+	def destroy
+		@event = Event.find(params[:id])
+		@event.destroy
+		redirect_to events_path		
+	end
+	private
+		def event_params
+			params.require(:event).permit(:name, :description, :location, :price, :start_at)
+		end
 end
