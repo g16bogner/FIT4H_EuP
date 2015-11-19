700c97554fb070701c8679a1b183538992e66a1c
g16bogner <g16bogner@gmail.com>
2015-11-19 14:41:38 +0100

    forms

--- rails/rails_app/app/controllers/events_controller.rb
+++ rails/rails_app/app/controllers/events_controller.rb
@@ -1,7 +1,7 @@
 class EventsController < ApplicationController
 
 	def index
-		@events = Event.all 
+		@events = Event.upcoming 
 	end
 
 	def show
--- rails/rails_app/app/models/event.rb
+++ rails/rails_app/app/models/event.rb
@@ -3,4 +3,8 @@ class Event < ActiveRecord::Base
 	def free?
 		self.price.blank? || self.price.zero? 
 	end
+
+	def self.upcoming
+		where("start_at >= ?", Time.now).order("start_at")
+	end
 end
