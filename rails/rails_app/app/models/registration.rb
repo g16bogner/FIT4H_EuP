ffffe75ef0794652d5d248c727df7d0e08a53d39
g16bogner <g16bogner@gmail.com>
2015-12-17 14:07:23 +0100

    associations2

--- rails/rails_app/app/models/registration.rb
+++ rails/rails_app/app/models/registration.rb
@@ -1,3 +1,7 @@
 class Registration < ActiveRecord::Base
   belongs_to :event
+  HOW_HEARD_OPTIONS = ['Newsletter', 'Twitter', 'Web Search', 'Other']
+
+  validates :how_heard, inclusion: { in: HOW_HEARD_OPTIONS }
+
 end
--- rails/rails_app/config/routes.rb
+++ rails/rails_app/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :registrations
+  
   root "events#index"
+  
   resources :events do
+    resources :registrations
     collection do
       get "showall"
     end
