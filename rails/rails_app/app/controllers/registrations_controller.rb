2c016dd9c66255481e128697865ea8b2fead0350
g16bogner <g16bogner@gmail.com>
2015-12-18 09:30:56 +0100

    Associations3

--- rails/rails_app/app/controllers/registrations_controller.rb
+++ rails/rails_app/app/controllers/registrations_controller.rb
@@ -1,6 +1,30 @@
 class RegistrationsController < ApplicationController
+	before_action :set_event
 	def index
-		@event = Event.find(params[:event_id])
+		
 		@registrations = @event.registrations
 	end
+
+	def new
+		@registration = @event.registrations.new
+	end
+
+	def create
+		@registration = @event.registrations.new(registration_params)
+		if @registration.save
+			#flash[:notice] = "Registrierung erfolgreich!"
+			redirect_to event_path(@event.id), notice: "Registrierung erfolgreich!"
+		else
+			flash[:alert] = "Penner, du musst eingeben woher du den Event kennst."
+			render "new"
+		end
+	end
+
+	private
+		def registration_params
+			params.require(:registration).permit(:name, :email, :how_heard)
+		end
+		def set_event
+			@event = Event.find(params[:event_id])
+		end
 end
